fad80a0ecfd5ccb45d62f9613c5401592c3b611c
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-08-19 12:19:00 -0600

    Add basic list structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import _ from 'lodash';
 
- function component() {
-   const element = document.createElement('div');
+
+class ListElement {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
+
+
+ function list() {
+   let todoList=[];
+
+   const ul = document.createElement('ul');
+
+
+   todoList.push(new ListElement('Do one thing', false, 0));
+   todoList.push(new ListElement('Do another thing', false, 1));
+   todoList.push(new ListElement('Do onelast thing', false, 3));
+
+   for (var i = 0; i < todoList.length; i++) {
+     const element = document.createElement('li');
+     element.textContent = todoList[i].description;
+     ul.append(element);
+
+   }
 
   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   return element;
+   return ul;
  }
 
-document.body.appendChild(component());
+document.body.appendChild(list());
