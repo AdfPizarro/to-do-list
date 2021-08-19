5455e8fd6ea18b6a3391ff38a2df4bcd7885b728
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-08-19 15:16:33 -0600

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import _ from 'lodash';
+import _ from 'lodash'; // eslint-disable-line no-unused-vars
 import './style.css';
-
 
 class ListElement {
   constructor(description, completed, index) {
@@ -11,42 +10,38 @@ class ListElement {
 }
 
 function list() {
-   let todoList=[];
+  const todoList = [];
 
-   const ul = document.createElement('ul');
+  const ul = document.createElement('ul');
 
+  todoList.push(new ListElement('Do one thing', false, 0));
+  todoList.push(new ListElement('Do another thing', false, 1));
+  todoList.push(new ListElement('Do one last thing', false, 3));
 
-   todoList.push(new ListElement('Do one thing', false, 0));
-   todoList.push(new ListElement('Do another thing', false, 1));
-   todoList.push(new ListElement('Do one last thing', false, 3));
+  for (let i = 0; i < todoList.length; i++) { // eslint-disable-line no-plusplus
+    const box = document.createElement('input');
+    box.setAttribute('type', 'checkbox');
 
-   for (var i = 0; i < todoList.length; i++) {
-     const box= document.createElement('input');
-     box.setAttribute('type', 'checkbox');
+    const textContainer = document.createElement('div');
+    textContainer.textContent = todoList[i].description;
 
-     const textContainer=document.createElement('div');
-     textContainer.textContent = todoList[i].description
+    const element = document.createElement('li');
 
-     const element = document.createElement('li');
+    const drag = document.createElement('i');
+    drag.setAttribute('class', 'fas fa-ellipsis-v');
 
+    element.setAttribute('class', 'listElement');
 
-     const drag = document.createElement('i');
-     drag.setAttribute('class', 'fas fa-ellipsis-v');
+    element.appendChild(box);
+    element.appendChild(textContainer);
+    element.appendChild(drag);
 
-     element.setAttribute('class', 'listElement');
-
-
-     element.appendChild(box);
-     element.appendChild(textContainer);
-     element.appendChild(drag);
-
-
-     ul.append(element);
-   }
+    ul.append(element);
+  }
 
   // Lodash, now imported by this script
 
-   return ul;
+  return ul;
 }
 
 const listContainer = document.getElementById('todoList');
