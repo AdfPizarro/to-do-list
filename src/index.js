6ec25fe3787fc30fa24a3afbe81c3f44e14a2d93
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-08-19 14:57:03 -0600

    Add styling to the list

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ class ListElement {
   }
 }
 
-
- function list() {
+function list() {
    let todoList=[];
 
    const ul = document.createElement('ul');
@@ -19,19 +18,36 @@ class ListElement {
 
    todoList.push(new ListElement('Do one thing', false, 0));
    todoList.push(new ListElement('Do another thing', false, 1));
-   todoList.push(new ListElement('Do onelast thing', false, 3));
+   todoList.push(new ListElement('Do one last thing', false, 3));
 
    for (var i = 0; i < todoList.length; i++) {
-     const element = document.createElement('li');
-     element.textContent = todoList[i].description;
-     ul.append(element);
+     const box= document.createElement('input');
+     box.setAttribute('type', 'checkbox');
 
+     const textContainer=document.createElement('div');
+     textContainer.textContent = todoList[i].description
+
+     const element = document.createElement('li');
+
+
+     const drag = document.createElement('i');
+     drag.setAttribute('class', 'fas fa-ellipsis-v');
+
+     element.setAttribute('class', 'listElement');
+
+
+     element.appendChild(box);
+     element.appendChild(textContainer);
+     element.appendChild(drag);
+
+
+     ul.append(element);
    }
 
   // Lodash, now imported by this script
 
    return ul;
- }
+}
 
 const listContainer = document.getElementById('todoList');
 listContainer.appendChild(list());
